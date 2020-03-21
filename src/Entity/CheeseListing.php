5eaed8bd8b51fd0472231eda687d706c6c499a11
manu <napestershine@gmail.com>
2020-03-21 15:52:31 +0100

    chapter - 13 Added Pagination

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -25,7 +25,10 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *     },
  *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
  *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
- *     shortName="cheeses"
+ *     shortName="cheeses",
+ *     attributes={
+ *      "pagination_items_per_page = 20
+ *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
